f46327cc073d948be4841092a3b5c1e971a282b4
Rishav Kumar <rishavkr43125@gmail.com>
2025-09-21 01:02:29 +0530

    delpoyment changes

--- backend/app/main.py
+++ backend/app/main.py
@@ -74,5 +74,6 @@ def health():
 
 
 if __name__ == "__main__":
-    # For local dev: python -m app.main
-    uvicorn.run("app.main:app", host="127.0.0.1", port=8000, reload=True)
+    port = int(os.environ.get("PORT", 8000))
+    # for deployment on cloud environment.
+    uvicorn.run("app.main:app", host="0.0.0.0", port=port, log_level="info")
